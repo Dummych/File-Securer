746b5355cc7bbf14a79bc47d7bd21340ccc93384
Dummych <qwrawp@gmail.com>
2026-07-12 12:25:42 +0500

    Decrypt improvments + little visual performance

--- pyt/enc.py
+++ pyt/enc.py
@@ -34,10 +34,30 @@ def get_key(psw, enc_salt):
     key = derive_key(psw, enc_salt)  
     fer = Fernet(key)
 
+def encrypt_data(user, u_files, pth):
+    u_pth = BASE_DIR.parent / "data" / user
+    if pth == None:    
+            folder = BASE_DIR.parent / "enc" 
+            has_files = any(f.is_file() for f in folder.iterdir())
+            if has_files:    
+                for file, data in check_folder(folder):
+                    file = Path(file)
+                    raw_data = data
+                    encrypt_data_func(u_pth, u_files, file, raw_data)
+            elif not has_files:
+                deb_print("dir_none", Path(folder).name)
+    else:
+        file = Path(pth)    
+        if file.exists():    
+            with open(file, "rb") as f:
+                raw_data = f.read()
+            encrypt_data_func(u_pth, u_files, file, raw_data)
+            file.unlink(missing_ok=True)
+        else:
+            deb_print("dir_pth_error", None)
+           
 @timer
-def encrypt_data_func(u_pth, u_files, folder):   
-    for file, raw_data in check_folder(folder):
-        file = Path(file)
+def encrypt_data_func(u_pth, u_files, file, raw_data):   
         deb_print("enc_in", file.name)
         enc_data = fer.encrypt(raw_data)
         enc_file = to_enc(file, u_files)
@@ -46,48 +66,34 @@ def encrypt_data_func(u_pth, u_files, folder):
             f_enc.write(enc_data)
         deb_print("enc_out", file.name)       
 
-@timer
-def encrypt_data_pth_func(file, u_pth, u_files):
-    deb_print("enc_in", file)
-    with open(file, "rb") as f:
-        raw_data = f.read()
-    enc_data = fer.encrypt(raw_data)
-    enc_file = to_enc(file, u_files)
-    f_pth = u_pth / enc_file
-    with open(f_pth, "wb") as f_enc:
-        f_enc.write(enc_data)
-    deb_print("enc_out", file.name)    
-
-def encrypt_data(user, u_files, pth):
-    u_pth = BASE_DIR.parent / "data" / user
-    if pth == None:    
-            folder = BASE_DIR.parent / "enc" 
-            has_files = any(f.is_file() for f in folder.iterdir())
-            if has_files:    
-                encrypt_data_func(u_pth, u_files, folder)
-            elif not has_files:
-                deb_print("dir_none", Path(folder).name)
-    else:
-        file = Path(pth)    
-        if file.exists():    
-            encrypt_data_pth_func(file, u_pth, u_files)
-   
-        else:
-            deb_print("dir_pth_error", None)
-           
-
-
-@timer    
 def decrypt_data(user, u_files, pth):
     folder = BASE_DIR.parent / "data" / user
+    if pth == None:
+        dir = BASE_DIR.parent / "dec"
+        decrypt_data_func(u_files, folder, dir)
+    else:
+        dir = Path(pth)
+        if dir.exists():
+            decrypt_data_func(u_files, folder, dir)
+        elif not dir.exists():
+            deb_print("dir_pth_error", None)
+
+@timer
+def decrypt_data_func(u_files, folder, dir_pth):
     for file, raw_data in check_folder(folder):
         deb_print("dec_in", Path(file).name)
         enc_data = raw_data
         dec_data = fer.decrypt(enc_data)
         n_file = to_norm(file, u_files)
         u_files.pop(Path(file).stem)
-        f_pth = BASE_DIR.parent / "dec" / n_file
+        f_pth = dir_pth / n_file
         with open(f_pth, "wb") as f_dec:
             f_dec.write(dec_data)
-        deb_print("dec_out", Path(file).name)           
+        deb_print("dec_out", Path(file).name) 
+
+
+
+
+
+
           
